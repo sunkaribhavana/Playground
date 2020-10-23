ac95683efd8c4882358fb406f9ca8f00f9a22d61
sunkaribhavana <sunkaribhavana24@gmail.com>
2020-10-23 15:29:27 +0530

    Test code pushed to repository.

---
+++ GCD of 2 numbers/Main.java
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int n1,n2,gcd;
+  cin>>n1;
+  cin>>n2;
+  for(int i=1;i<=n1 && i<=n2;++i)
+  {
+  if(n1%i==0 && n2%i ==0)
+    gcd=i;
+  
+  }
+  cout<<gcd;
+  return 0;
+}
---
+++ Square root of a Number/Main.java
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main() 
+{
+  int n;
+  cin>>n;
+  double x=0.5;
+  double result;
+  result=pow(n,0.5);
+  printf("%.2lf",result);
+  
+  
+  
+    return 0;
+}
